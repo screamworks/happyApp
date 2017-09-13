ab5693cf8a2a4d5765b7a2f9618b8b1fea2c610e
Shiva Patel <shivapatel15@yahoo.com>
2017-09-13 01:37:58 -0400

    inserted routing blueprint

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-angular.module('hotspot',[]);
+angular.module('hotspot',["ui.router"])
+.config(function($stateProvider, $urlRouterProvider){
+
+    $stateProvider
+    .state("home", {
+      url: "/",
+      templateUrl: "../index.html"
+    })
+
+    .state("about", {
+      url: "/",
+      templateUrl: "../views/about.html",
+      controller: "aboutCtrl"
+    })
+
+    .state("factpage", {
+      url: "/",
+      templateUrl: "../views/factpage.html",
+      controller: "factpageCtrl"
+    })
+
+    .state("linechart", {
+      url: "/",
+      templateUrl: "../views/linechart.html",
+      controller: "linechartCtrl"
+    })
+
+    .state("newsfeed", {
+      url: "/",
+      templateUrl: "../views/newsfeed.html",
+      controller: "newsfeedCtrl"
+    })
+
+    .state("piechart", {
+      url: "/piechart",
+      templateUrl: "../views/piechart.html",
+      controller: "piechartCtrl"
+    })
+
+$urlRouterProvider.otherwise("/");
+
+
+
+});
