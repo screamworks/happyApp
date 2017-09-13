be8223ea67227c5e7a61ed4b017f99d0559e9354
Shiva Patel <shivapatel15@yahoo.com>
2017-09-12 21:45:15 -0400

    added sketch of chart

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -14,5 +14,54 @@ promise2.then(function(data){
   console.log($scope.getNews)
 })
 
+var myChart = document.getElementById('myChart').getContext('2d')
+Chart.defaults.global.defaultFontFamily =  'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+var lineChart = new Chart(myChart, {
+  type: 'line',
+  data: {
+    labels:['Points of Satisfaction'],
+    dataSets: [{
+      label: "In Years",
+      data: [
+        200000,
+        150000,
+        175000
+      ],
+      // backgroundColor: 'green'
+      backgroundColor: [
+        'red',
+        'green',
+        'blue'
+      ]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Happiness Levels Measured',
+      fontSize: 25
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+
+      }
+    },
+    layout: {
+      padding: {
+        left: 50,
+        right: 0,
+        bottom: 0,
+        top: 0
+      }
+    },
+    tooltips:{
+      enabled: true
+    }
+  }
+})
 
 });
